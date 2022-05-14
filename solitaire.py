169693d9107e469c72097aff2d647cf502bdc6ef
John S. Rajeski, Ed.D <48130054+rajeski@users.noreply.github.com>
2022-05-14 23:31:46 +0900

    Code Updates
    Continued coding this Python-file.

--- solitaire.py
+++ solitaire.py
@@ -2,4 +2,13 @@ import random
 BREAK_STRING = "-----------------------------------------------------"
 
 class Card():
-    card_to_name ={}
+    card_to_name = {1:"A", 2:"2", 3:"3", 4:"4", 5:"5", 6:"6", 7:"7",
+                    8:"8", 9:"9", 10:"10", 11:"J", 12:"Q", 13:"K"}
+
+    def __init__(self, value, suit): 
+        self.name = self.card_to_name[value]
+        self.suit = suit
+        self.title = "%s%s" % (self.name, self.suit)
+        self.value = value
+
+
